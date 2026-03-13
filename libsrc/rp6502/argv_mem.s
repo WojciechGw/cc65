bf0675715b1506611aa5ffff000637f927ade988
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2026-03-12 23:07:23 -0700

    huh

--- libsrc/rp6502/argv_mem.s
+++ libsrc/rp6502/argv_mem.s
@@ -1,6 +1,6 @@
 ;
 ; Default argv_mem: returns NULL so argc/argv are silently skipped.
-; Override by providing storage for argv:
+; Override by providing storage for argv, e.g.
 ; void *__fastcall__ argv_mem(size_t size) { return malloc(size); }
 ;
 
