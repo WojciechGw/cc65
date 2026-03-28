596d965549a9a4dc2f6cf8c6342db5f4517150e4
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2026-03-27 21:41:13 -0700

    attr

--- libsrc/rp6502/ria_execv.c
+++ libsrc/rp6502/ria_execv.c
@@ -2,9 +2,9 @@
 #include <errno.h>
 #include <string.h>
 
-int __fastcall__ ria_execv (const char *path, char * const argv[])
+int __fastcall__ ria_execv (const char* path, char* const argv[])
 {
-    const char *ptrs[16];
+    const char* ptrs[16];
     unsigned int lens[16];
     int argc;
     unsigned int total_str;
@@ -13,7 +13,7 @@ int __fastcall__ ria_execv (const char *path, char * const argv[])
 
     /* path is prepended as argv[0]; the caller's argv[] becomes argv[1]+. */
     ptrs[0] = path;
-    lens[0] = (unsigned int)strlen(path) + 1U;
+    lens[0] = (unsigned int)strlen (path) + 1U;
     argc = 1;
     total_str = lens[0];
 
@@ -25,7 +25,7 @@ int __fastcall__ ria_execv (const char *path, char * const argv[])
             return -1;
         }
         ptrs[argc] = argv[i];
-        lens[argc] = (unsigned int)strlen(argv[i]) + 1U;
+        lens[argc] = (unsigned int)strlen (argv[i]) + 1U;
         total_str += lens[argc];
         if (total_str > 512U) {
             errno = EINVAL;
@@ -55,7 +55,7 @@ int __fastcall__ ria_execv (const char *path, char * const argv[])
      */
     for (i = argc - 1; i >= 0; --i) {
         for (j = (int)lens[i] - 1; j >= 0; --j) {
-            ria_push_char(ptrs[i][j]);
+            ria_push_char (ptrs[i][j]);
         }
     }
 
@@ -66,12 +66,12 @@ int __fastcall__ ria_execv (const char *path, char * const argv[])
      *
      * Offset[i] = int_table_size + sum(lens[0..i-1]).
      */
-    ria_push_int(0);
+    ria_push_int (0);
     offset = (unsigned int)(argc + 1) * 2U + total_str;
     for (i = argc - 1; i >= 0; --i) {
         offset -= lens[i];
-        ria_push_int(offset);
+        ria_push_int (offset);
     }
 
-    return ria_call_int(RIA_OP_EXEC);
+    return ria_call_int (RIA_OP_EXEC);
 }
